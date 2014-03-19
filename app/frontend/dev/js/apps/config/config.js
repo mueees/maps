b152199b80a77eb72ac3f440bdb1b2293e70d98a
mue <mue.miv@gmail.com>
2014-03-19 10:33:18 +0200

    add html and css for index

---
+++ app/frontend/dev/js/apps/config/config.js
@@ -0,0 +1,8 @@
+define([], function(){
+    return {
+        api: {
+            'userSignUp': '/api/user/signUp',
+            'userSignIn': '/api/user/signIn'
+        }
+    }
+})
--- app/frontend/dev/js/apps/home/home.js
+++ app/frontend/dev/js/apps/home/home.js
@@ -2,6 +2,8 @@ require([
     'apps/app',
     'routefilter',
 
+    'helpers/notify/module',
+
     /*modules*/
     'home/modules/auth/auth_app',
     'home/modules/map/module_app'
